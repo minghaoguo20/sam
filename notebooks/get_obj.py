4f30690db3615b14c19bdadd9a20e54b0574c74e
Henry <ihenrykwok@outlook.com>
2023-08-05 22:59:16 +0800

    add translation

---
+++ notebooks/get_obj.py
@@ -0,0 +1,48 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import cv2
+
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+
+image = cv2.imread('images/dog.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+import sys
+sys.path.append("..")
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+
+sam_checkpoint = "sam_vit_h_4b8939.pth"
+model_type = "vit_h"
+device = "cuda"
+
+sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+sam.to(device=device)
+
+mask_generator = SamAutomaticMaskGenerator(
+    model=sam,
+    points_per_side=32,
+    pred_iou_thresh=0.86,
+    stability_score_thresh=0.92,
+    crop_n_layers=1,
+    crop_n_points_downscale_factor=2,
+    min_mask_region_area=100,  # Requires open-cv to run post-processing
+)
+
+masks = mask_generator.generate(image)
+
+# [ ] TODO: 取出最大的那个mask
+mask = masks[0]
